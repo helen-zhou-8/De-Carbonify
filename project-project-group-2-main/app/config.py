68ad7efddb507a3ba803fb28ee57e91bb383392c
helen-zhou-8 <106274866+helen-zhou-8@users.noreply.github.com>
2023-06-12 14:47:44 -0400

    Delete project-project-group-2-main directory

--- project-project-group-2-main/app/config.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-#SQLAlchemy Setup
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-#Testing
-# app.config['LOGIN_DISABLED'] = True
-
